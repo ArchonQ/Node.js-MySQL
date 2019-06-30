695ead7776a5a5880220b492fed949978a789aa1
ArchonQ <brandon.cypaso@gmail.com>
2019-06-30 13:50:31 -0500

    updated sql

--- bamazon.sql
+++ bamazon.sql
@@ -5,7 +5,7 @@ USE bamazon_db;
 
 CREATE TABLE products
 (
-item_id INT NOT NULL AUTO_INCREMENT,
+item_id INT AUTO_INCREMENT NOT NULL,
 product_name VARCHAR(100) NOT NULL,
 department_name VARCHAR (100) NOT NULL,
 price DECIMAL (10,2) NOT NULL,
